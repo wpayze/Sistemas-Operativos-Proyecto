9db549d92b91bdb327406a8f1399fa2db53ae497
wpayze <will_paiz@hotmail.com>
2017-08-27 23:57:15 -0600

    proyect 3

---
+++ project3/compileOS.bash
@@ -0,0 +1,33 @@
+#!/bin/bash 
+
+echo "Borrando Archivos"
+rm bootload
+rm floppya.img
+rm kernel
+rm kernel.o
+rm kernel_asm.o
+
+echo "Compilando Bootloader"
+nasm bootload.asm 
+
+echo "Creando FLoppy Disk"
+dd if=/dev/zero of=floppya.img bs=512 count=2880 
+dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc seek=0 
+dd if=map.img of=floppya.img bs=512 count=1 seek=1 conv=notrunc
+dd if=dir.img of=floppya.img bs=512 count=1 seek=2 conv=notrunc
+
+echo "Compilando Kernel"
+bcc -ansi -c -o kernel.o kernel.c 
+as86 kernel.asm -o kernel_asm.o 
+ld86 -o kernel -d kernel.o kernel_asm.o 
+
+echo "Compilando el Uprog"
+as86 lib.asm -o lib.o
+bcc -ansi -c -o uprog1.o uprog1.c
+ld86 -o uprog1 -d uprog1.o lib.o
+./loadfile uprog1
+
+echo "Cargando Kernel"
+dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3 
+
+echo "Procesos Finalizados."
--- project3/kernel.c
+++ project3/kernel.c
@@ -1,20 +1,21 @@
-
 void putChar(char car, byte col, int fil, int col);
 void putStr(char* str, byte col, int fil, int col);
 int printString(char *str);
 int readChar();
 int readString(char *buf);
 int readSector(char *buf, int absSector);
+int readfile(char *filename, char *buf);
+void executeProgram(char* name, int segment); 
+int strComp(char* buf, char* fileName);
+void terminate();
+void resetSegments();
 
 int main()
 {
-
 	makeInterrupt21();
-	interrupt(0x21,0x00,0,0,0);
-while(1)
-{
-};
-
+	interrupt(0x21, 0x04, "uprog1\0", 0x2000, 0);
+	interrupt(0x21, 0x00, "Done!\n\r\0", 0, 0);
+	while(1);
 }
 
 int MOD(int x, int y) 
@@ -124,8 +125,8 @@ return 0;
 int readSector(char *buf, int absSector)
 {
 	//3.5"
-	int relSector = mod(absSector,18) + 1;
-	int head = mod((absSector/18 ), 2);
+	int relSector = MOD(absSector,18) + 1;
+	int head = MOD((absSector/18 ), 2);
 	int track = ( absSector / 36 );
 
 	int ax = 0x02 * 256 + 0x01;
@@ -160,4 +161,113 @@ int handleInterrupt21(int ax, int bx, int cx, int dx)
 	return -1;
 }
 
+int readFile(char* fileName, char* buf)
+{ 
+        int file=0;
+        int name = 0;
+	int index, h, k, i;
+	int sectores[27];
+	int j = 0;
+	int buffAddress = 0;
+	
+    	readSector(buf, 2);  
 
+	file = strComp(buf,fileName);
+
+	if (file != 0)
+	{
+		index = file*32+6;
+
+		for (j=0; j<26; j++)
+		{
+			sectores[j] = buf[index+j];
+		}
+
+		sectores[26] = 0;
+		k = 0;
+
+		while(sectores[k]!=0x0)
+		{
+			readSector(buf + buffAddress, sectores[k]);
+			buffAddress += 512;
+			k++;
+		}
+		
+	}
+	else
+	{
+		printString("No se encontro el archivo!");
+		return -1;
+	}		
+
+}
+
+int strComp(char* buf, char* fileName)
+{ 
+	int i, j;
+
+	int check = 0;
+
+
+     	for (i = 0; i < 16; i++)
+ 	{
+		if (buf[32*i] != 0x0)
+		{
+			for (j=0; j < 6; j++)
+			{
+				if (buf[j+32*1] == 0x0 || buf[j+32*1] == '\r' || buf[j+32*1] == '\n' || fileName[j] == 0x0 || fileName[j] == '\r' || fileName[j] == '\n')
+				{
+					break;
+				}
+				else if (buf[j+32*i] == fileName[j]){
+					check = 1;	
+				}
+				else {
+					check = 0;
+					break;
+				}
+				
+			}
+		 	
+			if (check == 1){
+
+				 return i;
+			}
+		}
+		}
+			if (check == 0)
+			{
+				 for (i=0; i<13312; i++)
+				 {
+					buf[i] = 0x0;
+				 }	 
+				return 0;
+			 }
+}
+
+void executeProgram(char* name, int segment)
+{ 
+	int i;
+	char readingBuffer[13312];
+
+	readFile(name, readingBuffer);
+
+	for (i=0; i<13312; i++)
+	{
+		putInMemory(segment, i, readingBuffer[i]);
+	} 
+
+	launchProgram(segment);
+}
+
+void terminate()
+{
+	char shell[6];
+	shell[0] = 's';
+	shell[1] = 'h';
+	shell[2] = 'e';
+	shell[3] = 'l';
+	shell[4] = 'l';
+	shell[5] = 0x0;
+	interrupt(0x21, 4, shell, 0x2000,0);
+}
